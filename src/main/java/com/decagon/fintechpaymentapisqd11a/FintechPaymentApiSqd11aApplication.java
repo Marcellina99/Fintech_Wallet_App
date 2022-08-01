c055fc1f36e6345ea91ddaf10bd619f090aee93b
David Baba <davidbabasalifu2020.com>
2022-08-01 22:54:41 +0100

    Created initial project folder

---
+++ src/main/java/com/decagon/fintechpaymentapisqd11a/FintechPaymentApiSqd11aApplication.java
@@ -0,0 +1,13 @@
+package com.decagon.fintechpaymentapisqd11a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintechPaymentApiSqd11aApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FintechPaymentApiSqd11aApplication.class, args);
+	}
+
+}

cb27bef59771c6e2aaf5cf1debf1b42fc4fb2d85
calebsamuelfc <samcalluis@gmail.com>
2022-08-11 21:12:27 +0100

    test

--- src/main/java/com/decagon/fintechpaymentapisqd11a/services/WalletService.java
+++ src/main/java/com/decagon/fintechpaymentapisqd11a/services/WalletService.java
@@ -1,11 +1,8 @@
 package com.decagon.fintechpaymentapisqd11a.services;
 
-import com.decagon.fintechpaymentapisqd11a.dto.WalletDto;
 import org.springframework.stereotype.Service;
-
-import java.nio.file.attribute.UserPrincipalNotFoundException;
 
 @Service
 public interface WalletService {
-    WalletDto viewWalletDetails(Long UserId) throws UserPrincipalNotFoundException;
+//    WalletDto viewWalletDetails(Long UserId) throws UserPrincipalNotFoundException;
 }

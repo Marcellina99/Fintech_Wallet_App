8d5881a4aa3e05b77eeb4c5dcf97631b83333122
EllaHacks <ikeemmanuelachinwe@gmail.com>
2022-08-16 10:44:00 +0100

    Get list of banks

---
+++ src/main/java/com/decagon/fintechpaymentapisqd11a/services/TransferService.java
@@ -0,0 +1,9 @@
+package com.decagon.fintechpaymentapisqd11a.services;
+
+import com.decagon.fintechpaymentapisqd11a.models.FlwBank;
+import java.util.List;
+
+
+public interface TransferService {
+    List<FlwBank> getAllBanks();
+}
--- src/main/java/com/decagon/fintechpaymentapisqd11a/util/Constant.java
+++ src/main/java/com/decagon/fintechpaymentapisqd11a/util/Constant.java
@@ -5,6 +5,7 @@ public class Constant {
     public static final String  CREATE_VIRTUAL_ACCOUNT_API = "https://api.flutterwave.com/v3/virtual-account-numbers";
     public static final String EMAIL_VERIFICATION_TOKEN_LINK = "http://localhost:3333/api/v1/user/confirm?token=";
     public static final String AUTHORIZATION = "FLWSECK_TEST-2f4e23ee51c1ba35a80337a3ae24b60a-X";
+    public static final String GET_ALL_BANKS = "https://api.flutterwave.com/v3/banks/NG";
 
 
 }
